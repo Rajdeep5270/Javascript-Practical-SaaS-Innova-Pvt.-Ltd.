454f8f697111c52c77a1139be7f47689a60afa7a
Rajdeep5270 <rajdeepex5270@gmail.com>
2026-07-14 15:02:26 +0530

    Simple Task Manager Application

---
+++ Basic Practical Tasks/Simple Task Manager Application/script.js
@@ -0,0 +1,111 @@
+let taskLists = [];
+
+// global variables used in 1 or more function 
+const tBody = document.getElementById("tBody");
+const tDescription = document.getElementById("tDescription");
+const tStatus = document.getElementById("tStatus");
+
+
+const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    // console.log(`Task Description = ${tDescription.value}`);
+    // console.log(`Task Status = ${tStatus.value}`);
+
+
+    taskLists.push({
+        id: Math.floor(Math.random() * 1000),
+        description: tDescription.value,
+        status: tStatus.value
+    })
+
+    // console.log(taskLists)
+
+    tDescription.value = "";
+
+    viewTaskLists();
+};
+
+const viewTaskLists = () => {
+
+    tBody.innerHTML = "";
+
+    // console.log(taskLists);
+
+    taskLists.map((task, idx) => {
+        tBody.innerHTML += ` <tr>
+                                    <td>${idx + 1}</td>
+                                    <td>${task.description}</td>
+                                    <td>
+                                        <button onclick="updateTask(${task.id})" style="background-color:${(task.status === "Completed") ? "green" : "red"}">${task.status}</button>
+                                    </td>
+                                    <td>
+                                        <button onclick="editTask(${task.id})">Edit</button>
+                                        <button onclick="deleteTask(${task.id})">Delete</button>
+                                    </td>
+                                </tr>`;
+    });
+};
+
+const deleteTask = (id) => {
+    // console.log("Task ID : ", id);
+
+    tBody.innerHTML = "";
+
+    taskLists = taskLists.filter(task => task.id !== id);
+
+    viewTaskLists(taskLists);
+};
+
+const editTask = (editId) => {
+    const editTask = taskLists.find(task => task.id === editId);
+
+    taskLists = taskLists.filter((task) => task.id !== editId);
+
+    tDescription.value = editTask.description;
+
+    viewTaskLists();
+};
+
+const updateTask = (updateId) => {
+    // console.log("Update ID : ", updateId);
+
+    const task = taskLists.find((task) => task.id === updateId);
+
+    // console.log("Task Status : ", task.status);
+
+    if (task.status === "Completed") {
+        task.status = "Pending"
+    } else {
+        task.status = "Completed"
+    }
+
+    // for debug 
+    // console.log("Task Status", task.status)
+
+    viewTaskLists();
+}
+
+const filterData = (filter) => {
+    // console.log("Filter : ", filter);
+
+    const filteredTasks = taskLists.filter(task => task.status.toLowerCase().includes(filter.toLowerCase()));
+
+    tBody.innerHTML = "";
+
+    filteredTasks.map((task, idx) => {
+        tBody.innerHTML += ` <tr>
+                                    <td>${idx + 1}</td>
+                                    <td>${task.description}</td>
+                                    <td>
+                                        <button onclick="updateTask(${task.id})" style="background-color:${(task.status === "Completed") ? "green" : "red"}">${task.status}</button>
+                                    </td>
+                                    <td>
+                                        <button onclick="editTask(${task.id})">Edit</button>
+                                        <button onclick="deleteTask(${task.id})">Delete</button>
+                                    </td>
+                                </tr>`;
+    });
+
+    // viewTaskLists(taskLists);
+}
