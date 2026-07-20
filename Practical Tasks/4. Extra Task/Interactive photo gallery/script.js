21e0e15e85143f39562d404b8701863df35d7a66
Rajdeep5270 <rajdeepex5270@gmail.com>
2026-07-20 15:39:01 +0530

    Interactive Photo Gallery

---
+++ Practical Tasks/4. Extra Task/Interactive photo gallery/script.js
@@ -0,0 +1,60 @@
+let allImages = [];
+
+const urlInput = document.getElementById("urlInput");
+const imageView = document.getElementById("imageView");
+const enlargedImage = document.getElementById("enlargedImage");
+
+const enlarged = document.getElementById("enlarged");
+
+function addImage(event) {
+    event.preventDefault();
+
+    allImages.push({
+        id: Math.floor(Math.random() * 1000),
+        imageUrl: urlInput.value
+    });
+
+    renderImages();
+
+    urlInput.value = "";
+}
+
+function renderImages() {
+    imageView.innerHTML = "";
+
+    allImages.forEach(img => {
+        imageView.innerHTML += `
+                    <div class="w-4 w-md-2 w-sm-1">
+                        <div class="gallery-content position-relative" id="imageContainer">
+                            <img src="${img.imageUrl}" alt="">
+                            <div class="image-overlay position-absolute d-flex">
+                                <button onclick="deleteImg(${img.id})">Delete</button>
+                                <button class="view" onclick="viewLargeImage(${img.id})">View</button>
+                            </div>
+                        </div>
+                    </div>
+            
+        `;
+    })
+}
+
+function deleteImg(deleteId) {
+    // console.log("Delete ID : ", deleteId);
+
+    allImages = allImages.filter(img => deleteId !== img.id);
+
+    renderImages();
+}
+
+function viewLargeImage(viewId) {
+    // console.log("View Image ID : ", viewId);
+    // enlarged.style.display = "flex";
+
+    const img = allImages.find(img => img.id === viewId);
+
+    enlargedImage.setAttribute('src', `${img.imageUrl}`);
+}
+
+function hideEnlargedImage() {
+    enlargedImage.setAttribute('src', '');
+}
