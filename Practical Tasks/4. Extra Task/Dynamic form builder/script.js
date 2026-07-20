8e340fbe2a0fc7deb2eb020f259527c5e72e6d91
Rajdeep5270 <rajdeepex5270@gmail.com>
2026-07-20 13:54:33 +0530

    Dynamic form builder

---
+++ Practical Tasks/4. Extra Task/Dynamic form builder/script.js
@@ -0,0 +1,16 @@
+let inputFields = +prompt("Enter number of input fields : ");
+let inputType = prompt("Enter element type : ");
+
+const displayInputFields = document.getElementById("displayInputFields");
+
+console.log(inputFields);
+console.log(inputType);
+
+// for (let i = 0; i < inputFields; i++) {
+//     displayInputFields.innerHTML += `
+//                     <div>
+//                         <label for="">Input</label>
+//                         <input type="text">
+//                     </div>
+//     `;
+// }
