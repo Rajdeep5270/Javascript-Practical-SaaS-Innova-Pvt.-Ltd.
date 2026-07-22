a5d8407884a8822523cac4112ee90a81531d445b
Rajdeep5270 <rajdeepex5270@gmail.com>
2026-07-22 14:03:21 +0530

    Dynamic Form Builder

--- Practical Tasks/4. Extra Task/Dynamic form builder/script.js
+++ Practical Tasks/4. Extra Task/Dynamic form builder/script.js
@@ -1,11 +1,44 @@
-const formInputType = document.getElementById("formInputType");
+
 const dynamicForm = document.getElementById("dynamicForm");
+const submitBtn = document.getElementById("submitBtn");
 
-document.getElementById("inputAddField").addEventListener('click', e => {
-    // alert("Hello World");
-    // console.log("Form Input Type Value : ", formInputType.value);
+function formCreation() {
+    // alert("This dynamic form includes heading, input, buttons, etc...");
+    let labelName = prompt("Enter Label Name");
+    let formInputType = prompt("Enter input type\nHeading\nText\nEmail\nPassword\nNumber\nRadio\nButton");
 
-    dynamicForm.innerHTML += `
-        <input type="${formInputType.value}"  placeholder="${formInputType.value}">
-    `;
-}); 
+    // console.log(labelName);
+    // console.log(formInputType);
+
+    if (!formInputType || !labelName) {
+        alert("Fill all fields");
+        return;
+    }
+
+    if (formInputType.toLowerCase() === "button") {
+        const button = document.createElement("button");
+        button.innerText = labelName;
+
+        submitBtn.appendChild(button);
+    } else if (formInputType.toLowerCase() === "heading") {
+        document.getElementById("formTitleDisplay").innerText = labelName;
+    } else if (formInputType === "radio") {
+        const label = document.createElement("label");
+        label.innerText = labelName;
+        const input = document.createElement("input");
+        input.setAttribute('type', `${formInputType}`);
+        input.setAttribute('value', `${labelName} `)
+
+        dynamicForm.appendChild(label);
+        dynamicForm.appendChild(input);
+    } else {
+        const label = document.createElement("label");
+        label.innerText = labelName;
+        const input = document.createElement("input");
+        input.setAttribute('type', `${formInputType} `);
+        input.setAttribute('placeholder', `Enter ${labelName} here`)
+
+        dynamicForm.appendChild(label);
+        dynamicForm.appendChild(input);
+    }
+}
